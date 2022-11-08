c50f44156016623a89558dc6e9f07babf791e8f6
ham-zettt <ilhamzakaria3024@gmail.com>
2022-11-08 23:39:45 +0700

    solved

---
+++ BNPC-HS/2022/A.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <vector>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+int main(){
+	int n, m, ans=0;
+	cin >> n >> m;
+
+	int arr[n];
+	memset(arr, 0, sizeof(arr));
+
+	for(int i=0; i<m; i++){
+		int temp;
+		cin >> temp;
+
+		for(int j=1; j<=n; j++){
+			if(j==temp){
+				arr[j-1]++;
+				break;
+			}
+		}
+	}
+
+	for(int i: arr){
+		if(i%4==2 || i%4==3){
+			ans++;
+		}
+	}
+	
+	cout << ans << endl;
+}
